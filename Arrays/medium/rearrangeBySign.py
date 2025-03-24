00b4b0dbc47d61e4b407426d3b7b97f313d5d181
harithas1 <163287893+harithas1@users.noreply.github.com>
2025-03-24 14:34:21 +0530

    rearrangeBySign.py

--- Arrays/medium/rearrangeBySign.py
+++ Arrays/medium/rearrangeBySign.py
@@ -1,4 +1,20 @@
-# Rearrange Array Elements by Sign
+# Rearrange Array Elements by Sign:
+
+
+# There’s an array ‘A’ of size ‘N’ with an equal number of positive and negative elements. Without altering the relative order of positive and negative elements, you must return an array of alternately positive and negative values.
+
+# Note: Start the array with positive elements.
+
+# Example:
+
+# Input:
+# arr[] = {1,2,-4,-5}, N = 4
+# Output:
+# 1 -4 2 -5
+
+
+
+# Leetcode: https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
 
 
 # Approach: 1(Brute Force)
